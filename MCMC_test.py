95483eeede0a13f1488acf027e5dd897a30cfec8
LightningMinion <46401302+LightningMinion@users.noreply.github.com>
2025-02-05 18:19:57 +0000

    Added MCMC algorithm

---
+++ MCMC_test.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(123)
+
+# Choose the "true" parameters.
+m_true = -0.9594
+b_true = 4.294
+f_true = 0.534
+
+# Generate some synthetic data from the model.
+N = 50
+x = np.sort(10 * np.random.rand(N))
+yerr = 0.1 + 0.5 * np.random.rand(N)
+y = m_true * x + b_true
+y += np.abs(f_true * y) * np.random.randn(N)
+y += yerr * np.random.randn(N)
+
+plt.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
+x0 = np.linspace(0, 10, 500)
+plt.plot(x0, m_true * x0 + b_true, "k", alpha=0.3, lw=3)
+plt.xlim(0, 10)
+plt.xlabel("x")
+plt.ylabel("y");
